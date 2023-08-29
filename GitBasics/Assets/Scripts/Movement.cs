a5850aae5c85c895e408824a7f81fe97e862ef71
sroberto27 <c00416436@louisiana.edu>
2023-08-29 16:14:23 -0500

    MOVE WITH ARROWS

--- GitBasics/Assets/Scripts/Movement.cs
+++ GitBasics/Assets/Scripts/Movement.cs
@@ -8,25 +8,25 @@ public class Movement : MonoBehaviour
     private void Update()
     {
         // Move Up
-        if (Input.GetKey(KeyCode.))// W or Up
+        if (Input.GetKey(KeyCode.UpArrow))// W or Up
         {
             transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
         }
 
         // Move Down
-        if (Input.GetKey(KeyCode.)) //S or Down
+        if (Input.GetKey(KeyCode.DownArrow)) //S or Down
         {
             transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
         }
 
         // Move Left
-        if (Input.GetKey(KeyCode.)) // A or Left
+        if (Input.GetKey(KeyCode.LeftArrow)) // A or Left
         {
             transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
         }
 
         // Move Right
-        if (Input.GetKey(KeyCode.)) // D or Right
+        if (Input.GetKey(KeyCode.RightArrow)) // D or Right
         {
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
         }
